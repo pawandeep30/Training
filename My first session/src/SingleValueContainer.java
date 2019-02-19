0ebf429c1ca5e140ecc4d3f5842cf480ecba5e73
Lenovo ideapad320 <Lenovo ideapad320@DESKTOP-E7KTJJA>
2019-02-19 10:47:22 +0530

    SESSION 1

---
+++ My first session/src/SingleValueContainer.java
@@ -0,0 +1,33 @@
+
+public class SingleValueContainer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     byte age=10;
+     
+     System.out.println("age is ="+age);
+     
+      age=15;
+     
+     System.out.println("age now is ="+age);
+     
+
+     age=20;
+     
+     System.out.println("age now is ="+age);
+     
+     
+
+		double salary = 345611.34;
+		
+		char ch = 'A';
+		ch = 66;
+		ch = '\u20b9';
+		System.out.println("ch is: "+ch);
+		
+		boolean isInternetConnected = true;
+		
+		isInternetConnected = false;
+	}
+
+}
